dc12c4357df705cfc8882b5472931c27c82214d0
Ngovi <44963314+ngovi2298@users.noreply.github.com>
2019-12-09 16:29:00 +0700

    add notification but have bug need fix

--- app/src/main/java/com/example/sleepz/Ringtone.java
+++ app/src/main/java/com/example/sleepz/Ringtone.java
@@ -23,6 +23,15 @@ public class Ringtone extends Service {
         Uri uriMusic = Uri.parse(intent.getStringExtra("pathMusic"));
         ringtone = MediaPlayer.create(this, uriMusic);
         ringtone.start();
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification.Builder noti  = new Notification.Builder(this)
+                .setContentTitle("")
+                .setContentText("Subject")
+                .setSmallIcon(R.drawable.icon_png_transparent)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0,noti);
         return START_NOT_STICKY;
     }
 }
