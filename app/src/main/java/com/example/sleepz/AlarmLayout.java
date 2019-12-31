64367eaf581434e9d1d6c34c37ab04d81345cfdd
TaiTrien <v.v.t.trien@gmail.com>
2019-12-31 17:40:08 +0700

    Fix bug of Tan  turn on lock screen

--- app/src/main/java/com/example/sleepz/AlarmLayout.java
+++ app/src/main/java/com/example/sleepz/AlarmLayout.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PersistableBundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Switch;
 
 import androidx.annotation.Nullable;
@@ -23,7 +25,12 @@ public class AlarmLayout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
         switchStop = findViewById(R.id.switchStop);
+        Window window = getWindow();
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         //getExtra();
     }
 
